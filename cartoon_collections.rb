07269f8aa80707fbe44738ec6fe7c315efcef752
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-13 12:59:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(array)
  array.each_with_index {|name,index| puts "/#{index+1}.*#{name}/"}
 end
 
-def summon_captain_planet(long_planeteer_calls)
-  long_planeteer_calls.collect {|x| x.capitalize + "!"}
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
