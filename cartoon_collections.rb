34d103f3060faab642bce2e09ff933f39e02e646
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-13 13:08:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
 def roll_call_dwarves(array)
- array.each_with_index {|name,index| puts "/#{index+1}.*#{name}/"}
+ array.each_with_index { |name,index| puts "/#{index+1}.*#{name}/" }
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|x| x.capitalize + "!"}
+  planeteer_calls.collect { |x| x.capitalize + "!" }
 end
 
 def long_planeteer_calls(calls)
-  calls.detect {|call| call.length > 4 } ? true : false
+  calls.detect { |call| call.length > 4 } ? true : false
 end
 
 def find_the_cheese(foods)
