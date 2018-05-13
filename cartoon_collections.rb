ff1481e12e6c7b6542ad0aa053a27cdb5de82974
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-13 13:07:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.detect {|call| call.length > 4 } ? true : false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.detect { |food| cheese_types.include? food }
 end
